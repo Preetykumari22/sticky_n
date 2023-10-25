9413495a3b27da7558e1e6ab34f59586338b1d7b
om prakash kumar <omjsr29@gmail.com>
2023-10-25 14:36:22 +0530

    sticky_notes

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+	import Note from './Note.svelte';
+	
+	let notes = [
+		{
+			id: 1,
+			text: 'welcome'
+		},
+		{
+			id: 2,
+			text: 'hello'
+		},
+    {
+			id: 3,
+			text: 'hii'
+		},
+		{
+			id: 4,
+			text: 'preety'
+		},
+    {
+			id: 5,
+			text: 'full stack'
+		},
+		{
+			id: 6,
+			text: 'bye'
+		},
+    {
+			id: 7,
+			text: 'nothing'
+		},
+		{
+			id: 8,
+			text: 'whaattt'
+		},
+    {
+			id: 9,
+			text: 'really'
+		},
+		{
+			id: 10,
+			text: 'How'
+		}
+	];
+</script>
+
+{#each notes as note}
+	<Note>
+		<div contenteditable>{note.text}</div>
+	</Note>
+{/each}
+
+<style>
+	div {
+		background: rgb(132, 132, 239);
+		padding: 20px;
+		width: 100px;
+		text-align: center;
+	}
+</style>
